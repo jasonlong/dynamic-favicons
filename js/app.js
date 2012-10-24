6703032b93440fcf8b364abe665df54dfc3d80e6
Jason Long <jlong@github.com>
2012-10-24 15:34:36 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var link          = $('#favicon').clone();
+var statuses      = ['green', 'orange', 'red'];
+var currentStatus = 0;
+var timer         = setInterval(switchFavicon, 3000);
+
+function switchFavicon() {
+  if (currentStatus === 3) currentStatus = 0;
+  // for Chrome
+  $('#favicon').attr({href: '/status-icon-'+statuses[currentStatus]+'.png'});
+  // for Firefox
+  $(link).attr({href: '/status-icon-'+statuses[currentStatus]+'.png'});
+  $('body').append(link);
+  currentStatus += 1;
+}
